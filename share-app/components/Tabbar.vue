f224f87443e6c7ec170d5d8392bf04468ba33650
gmth <143313748+gmth6789@users.noreply.github.com>
2023-09-25 10:53:51 +0000

    Create Tabbar.vue

---
+++ share-app/components/Tabbar.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="flex flex-wrap diygw-col-24 justify-around diygw-bottom tabbar-clz">
+					<div class="flex flex-wrap diygw-col-0 flex-direction-column items-center">
+						<span class="flex icon1 diygw-col-0 icon1-clz diy-icon-shop"></span>
+						<div class="diygw-col-0">หน้าหลัก</div>
+					</div>
+					<div class="flex flex-wrap diygw-col-0 flex-direction-column items-center">
+						<span class="flex icon2 diygw-col-0 icon2-clz diy-icon-star"></span>
+						<div class="diygw-col-0">ภารกิจ</div>
+					</div>
+					<div class="flex flex-wrap diygw-col-0 flex-direction-column items-center">
+						<span class="flex icon diygw-col-0 icon-clz diy-icon-message"></span>
+						<div class="diygw-col-0 text16-clz">10</div>
+						<div class="diygw-col-0">สมัคร</div>
+					</div>
+					<div class="flex flex-wrap diygw-col-0 flex-direction-column items-center">
+						<span class="flex icon4 diygw-col-0 icon4-clz diy-icon-people"></span>
+						<div class="diygw-col-0">ข้อมูล</div>
+					</div>
+				</div>
+</template>
+
+<style>
+
+</style>
