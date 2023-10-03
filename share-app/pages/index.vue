be088944ff406776f14c5a70453c3642639c8dad
gmth <143313748+gmth6789@users.noreply.github.com>
2023-10-03 15:51:40 +0000

    Update nuxt.config.js and index.vue

--- share-app/pages/index.vue
+++ share-app/pages/index.vue
@@ -1,69 +1,239 @@
 <template>
   <div>
-    <van-image
-      class="user-poster"
-      src="https://img.yzcdn.cn/public_files/2017/10/23/8690bb321356070e0b8c4404d087f8fd.png"
-    />
-    <van-row class="user-links">
-      <van-col span="6">
-        <van-icon name="pending-payment" />
-        待付款
-      </van-col>
-      <van-col span="6">
-        <van-icon name="records" />
-        待接单
-      </van-col>
-      <van-col span="6">
-        <van-icon name="tosend" />
-        待发货
-      </van-col>
-      <van-col span="6">
-        <van-icon name="logistics" />
-        已发货
-      </van-col>
-    </van-row>
-
-    <van-cell-group class="user-group">
-      <van-cell icon="records" title="全部订单" is-link />
-    </van-cell-group>
-
-    <van-cell-group>
-      <van-cell icon="points" title="我的积分" is-link />
-      <van-cell icon="gold-coin-o" title="我的优惠券" is-link />
-      <van-cell icon="gift-o" title="我收到的礼物" is-link />
-    </van-cell-group>
+    <v-app-bar
+      color="primary"
+      dense
+      flat
+      dark
+    >
+      <v-toolbar-title>สมัครสมาชิก</v-toolbar-title>
+    </v-app-bar>
+  <div class="home">
+    <h1 class="home__title">
+      Welcome to <br /><a
+        class="home__title__link"
+        href="https://developers.line.biz/en/docs/liff/overview/"
+        >LIFF Starter!</a
+      >
+    </h1>
+    <div class="home__badges">
+      <span class="home__badges__badge badge--primary"> LIFF Starter </span>
+      <span class="home__badges__badge badge--secondary"> nuxtjs </span>
+      <span class="home__badges__badge badge--primary"> {{ version }} </span>
+      <a
+        href="https://github.com/line/line-liff-v2-starter"
+        target="_blank"
+        rel="noreferrer"
+        class="home__badges__badge badge--secondary"
+      >
+        GitHub
+      </a>
+    </div>
+    <div class="home__buttons">
+      <a
+        href="https://developers.line.biz/en/docs/liff/developing-liff-apps/"
+        target="_blank"
+        rel="noreferrer"
+        class="home__buttons__button button--primary"
+      >
+        LIFF Documentation
+      </a>
+      <a
+        href="https://liff-playground.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+        class="home__buttons__button button--tertiary"
+      >
+        LIFF Playground
+      </a><NuxtLink to="/register">
+      <a
+        
+        target="_blank"
+        rel="noreferrer"
+        class="home__buttons__button button--secondary"
+      >
+       สมัครสมาชิก
+      </a></NuxtLink>
+    </div> </div>
   </div>
 </template>
 
-<style lang="less">
+<style>
+html,
 body {
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   margin: 0;
-  font-size: 16px;
-  background-color: #f8f8f8;
-  -webkit-font-smoothing: antialiased;
+  padding: 0;
 }
 
-.user {
-  &-poster {
-    width: 100%;
-    height: 53vw;
-    display: block;
-  }
+.home {
+  padding: 5rem 2rem;
+  text-align: center;
+}
 
-  &-group {
-    margin-bottom: 15px;
-  }
+.home__title {
+  font-size: 6rem;
+  line-height: 1.15;
+}
 
-  &-links {
-    padding: 15px 0;
+.home__title__link {
+  color: #06c755;
+  text-decoration: none;
+}
+
+.home__title__link:hover {
+  text-decoration: underline;
+}
+
+.home__badges {
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-wrap: nowrap;
+  overflow: hidden;
+  margin-bottom: 6rem;
+}
+
+.home__badges__badge:first-child {
+  border-top-left-radius: 2px;
+  border-bottom-left-radius: 2px;
+}
+
+.home__badges__badge:last-child {
+  border-top-right-radius: 2px;
+  border-bottom-right-radius: 2px;
+}
+
+.home__badges__badge {
+  display: inline-block;
+  padding: 0.3em 0.4em;
+  font-size: 0.75rem;
+  font-weight: 700;
+  line-height: 1;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: baseline;
+}
+
+.badge--primary {
+  color: #353a40;
+  border: 1px solid #353a40;
+  background-color: transparent;
+  padding-top: calc(0.3em - 1px);
+  padding-bottom: calc(0.3em - 1px);
+}
+
+.badge--secondary {
+  color: #fff;
+  background-color: #353a40;
+}
+
+.home__buttons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+.home__buttons__button {
+  min-width: 250px;
+  cursor: pointer;
+  display: inline-block;
+  font-weight: 400;
+  text-align: center;
+  vertical-align: middle;
+  user-select: none;
+  border: 1px solid transparent;
+  padding: 0.375rem 0.75rem;
+  font-size: 0.9375rem;
+  line-height: 1.5;
+  border-radius: 2px;
+  text-decoration: none;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+
+.button--primary {
+  color: #fff;
+  background-color: #00b900;
+  border-color: #00b900;
+}
+
+.button--primary:hover {
+  color: #fff;
+  background-color: #009300;
+  border-color: #008600;
+}
+
+.button--secondary {
+  color: #fff;
+  background-color: #353a40;
+  border-color: #353a40;
+}
+
+.button--secondary:hover {
+  color: #fff;
+  background-color: #24272b;
+  border-color: #1e2124;
+}
+
+.button--tertiary {
+  background-color: transparent;
+  background-image: none;
+  color: #353a40;
+  border-color: #353a40;
+}
+
+.button--tertiary:hover {
+  color: #353a40;
+  background-color: rgba(53, 58, 64, 0.1);
+  border-color: #353a40;
+}
+
+@media screen and (max-width: 600px) {
+  html {
     font-size: 12px;
-    text-align: center;
-    background-color: #fff;
+  }
 
-    .van-icon {
-      display: block;
-      font-size: 24px;
-    }
+  .home__title {
+    font-size: 4rem;
+    line-height: 1.15;
+  }
+
+  .home__buttons__button {
+    font-size: 1.5rem;
+  }
+}
+
+@media screen and (max-width: 930px) {
+  .home__buttons {
+    flex-direction: column;
   }
 }
 </style>
+
+<script>
+import packageJson from "../package.json";
+export default {
+  data: function() {
+    return {
+      version: packageJson.version,
+      sdkVersion: "",
+      liffError: ""
+    };
+  },
+  mounted() {
+    // mounted() is rendered when DOM is rendered
+    // wait liff.init()
+    this.$liffInit
+      .then(() => {
+        this.sdkVersion = liff.getVersion();
+      })
+      .catch((error) => {
+        this.liffError = error;
+      });
+  }
+};
+</script>
