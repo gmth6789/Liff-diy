ec5ee5f16a9bfe24061c97ff9c35339dc937406b
gmth <143313748+gmth6789@users.noreply.github.com>
2023-10-09 17:01:56 +0000

    Update index.vue

--- share-app/pages/register/index.vue
+++ share-app/pages/register/index.vue
@@ -84,7 +84,7 @@ export default {
   },  
   methods: {    
     isDone(){
-      this.$axios.get(`https://doonungonline-gm-default-rtdb.firebaseio.com/members/${this.$store.getters.getLine.userId}/profile.json`).then((res) => {
+      this.$axios.get(`https://auth-gmth-default-rtdb.asia-southeast1.firebasedatabase.app/members/${this.$store.getters.getLine.userId}/profile.json`).then((res) => {
         if(res.data != null){
           this.$router.push('register/done');
         }
