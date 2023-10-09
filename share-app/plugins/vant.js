f829bc3af0ee4ab41a7aac80844a41517385b8c9
gmth <143313748+gmth6789@users.noreply.github.com>
2023-10-09 17:16:04 +0000

    Update vant.js

--- share-app/plugins/vant.js
+++ share-app/plugins/vant.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import { Row, Col, Icon, Image, Cell, CellGroup } from 'vant';
+import { Row, Col, Icon, Image, Cell, CellGroup, Step, Steps } from 'vant';
 
 // 目前在 nuxt 中无法按需引入样式，因此采用手动引入的方式
 import 'vant/lib/index.css';
