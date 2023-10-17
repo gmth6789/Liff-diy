7c35daa725e269607ecd954b97a720accac37c7e
gmth <143313748+gmth6789@users.noreply.github.com>
2023-10-17 10:03:18 +0000

    Update Tabbar.vue

--- share-app/components/Tabbar.vue
+++ share-app/components/Tabbar.vue
@@ -6,15 +6,8 @@
   <van-tabbar-item icon="friends-o" badge="5">Tab</van-tabbar-item>
   <van-tabbar-item icon="setting-o" badge="20">Tab</van-tabbar-item>
 </van-tabbar>
-</template>
-	
 
-	<van-tabbar v-model="active">
-  <van-tabbar-item icon="home-o">标签</van-tabbar-item>
-  <van-tabbar-item icon="search" dot>标签</van-tabbar-item>
-  <van-tabbar-item icon="friends-o" badge="5">标签</van-tabbar-item>
-  <van-tabbar-item icon="setting-o" badge="20">标签</van-tabbar-item>
-</van-tabbar>
+
 </template>
 
 
